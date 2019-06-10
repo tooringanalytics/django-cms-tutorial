6389ec78e3c0360c21ae01b930c61dcf19e5b7af
Anshuman K <tooringtest@gmail.com>
2019-06-10 17:09:05 +0530

    fix(settings): Fixes Settings issues and adds documentation

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -31,12 +31,36 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # This provides some styling that helps make django CMS administration
+    # components easier to work with. Optional, but recommended.
+    'djangocms_admin_style',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    # cms and menus are the core django CMS modules.
+    'cms',
+    'menus',
+    # django-treebeard is used to manage django CMS’s page and
+    # plugin tree structures.
+    'treebeard',
+    # sekizai required by django-cms
+    'sekizai',
+    'filer',
+    'easy_thumbnails',
+    'mptt',
+    'djangocms_text_ckeditor',
+    'djangocms_link',
+    'djangocms_file',
+    'djangocms_picture',
+    'djangocms_video',
+    'djangocms_googlemap',
+    'djangocms_snippet',
+    'djangocms_style',
+    'djangocms_column',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +71,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Required by django-cms
+    'django.middleware.locale.LocaleMiddleware',
+    'cms.middleware.user.CurrentUserMiddleware',
+    'cms.middleware.page.CurrentPageMiddleware',
+    'cms.middleware.toolbar.ToolbarMiddleware',
+    'cms.middleware.language.LanguageCookieMiddleware',
+    'cms.middleware.utils.ApphookReloadMiddleware',
+
 ]
 
 ROOT_URLCONF = 'myproject.urls'
@@ -54,7 +86,9 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,6 +96,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # sekizai required by django-cms
+                'sekizai.context_processors.sekizai',
+                'cms.context_processors.cms_settings',
             ],
         },
     },
@@ -103,7 +140,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'mr'
 
 TIME_ZONE = 'UTC'
 
@@ -118,3 +156,35 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# django.contrib.sites, required by django-cms
+SITE_ID = 1
+
+# django CMS requires you to set the LANGUAGES setting. This should list all
+# the languages you want your project to serve, and must include the language
+# in LANGUAGE_CODE.
+
+LANGUAGES = [
+    ('mr', 'Marathi'),
+    ('hi', 'Hindi'),
+    ('en-us', 'English'),
+]
+
+# django CMS requires at least one template for its pages. The first template
+# in the CMS_TEMPLATES list will be the project’s default template.
+
+CMS_TEMPLATES = [
+    ('home.html', 'Home page template'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+THUMBNAIL_HIGH_RESOLUTION = True
+
+THUMBNAIL_PROCESSORS = (
+    'easy_thumbnails.processors.colorspace',
+    'easy_thumbnails.processors.autocrop',
+    'filer.thumbnail_processors.scale_and_crop_with_subject_location',
+    'easy_thumbnails.processors.filters'
+)
